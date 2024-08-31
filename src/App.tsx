b4bd72a82ef0c966ac5dde8ef9872297dfd84cfa
ahmednaser111 <ahmednaser7707@gmail.com>
2024-08-31 08:58:49 +0300

    entering valid data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import { Bell } from "lucide-react";
-import { useState } from "react";
+import {
+	Bell,
+	AlertCircle,
+	CheckCircle,
+	Info,
+	AlertTriangle,
+} from "lucide-react";
 import Alert from "./component/Alert/index";
 import "./index.css";
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Vite + React</h1>
+			<h1>Notification System</h1>
+
+			{/* Default Alert */}
+			<Alert
+				type="alert-default"
+				icon={<Bell />}
+				title="Default Alert"
+				desc="This is a default alert message. Everything is normal."
+			/>
+
+			{/* Success Alert */}
+			<Alert
+				type="alert-success"
+				icon={<CheckCircle />}
+				title="Success: Operation Completed"
+				desc="Your operation was completed successfully!"
+			/>
+
+			{/* Error Alert */}
 			<Alert
 				type="alert-error"
-				icon={<Bell />}
-				title="Something went wrong"
-				desc="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus dolorem magnam nemo id, totam obcaecati rerum omnis. Alias, minima tenetur?"
+				icon={<AlertCircle />}
+				title="Error: Something Went Wrong"
+				desc="We encountered an error while processing your request. Please try again later."
 			/>
-			<Alert type="alert-warning" icon={<Bell />} title="Warning">
-				<div>
-					<a href="#">try</a>
-				</div>
-			</Alert>
+
+			{/* Warning Alert */}
+			<Alert
+				type="alert-warning"
+				icon={<AlertTriangle />}
+				title="Warning: Check Your Input"
+				desc="Please review your input. Some fields might be missing or incorrect."
+			/>
+
+			{/* Info Alert */}
+			<Alert
+				type="alert-info"
+				icon={<Info />}
+				title="Info: System Update"
+				desc="The system will undergo maintenance tonight from 12:00 AM to 2:00 AM."
+			/>
 		</div>
 	);
 }
+
 export default App;
